4619833303abd2d6cb3180b66eef0355c28c3956
erikn775 <erikn775@gmail.com>
2020-10-30 19:52:03 -0600

    commit tree 22eb0544036f9d56637f3fe569631d130ba9db6c

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+    
+    attr_accessor :name, :roster, :add_student, :grade, :sort
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+
+    def add_student(name, grade)
+        array_of_students = []
+        if @roster[grade] == nil
+             @roster[grade] = array_of_students.push(name)
+        else
+             @roster[grade].push(name)
+        end
+
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+       
+        @roster.each {|k,v| @roster[k] = v.sort}
+        
+    end
+end
+
+
+
